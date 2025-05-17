15ec14afda5f5e939e003699898df1822f750ba5
Deepesh Gaharwar <deepeshgaharwar3@gmail.com>
2025-05-17 20:25:47 +0530

    final commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,16 +6,12 @@ const app = express();
 
 app.use(cors({origin : process.env.CORS_ORIGIN, credentials: true,}))
 
-// these middleware functions are used to parse incoming request bodies, and the limit parameter is used to restrict the size of the data being sent to the server
 app.use(express.json({limit: "16kb"}))
 
-// a middleware that enables the server to parse incoming URL-encoded form data
 app.use(express.urlencoded({extended : true, limit : "16kb"}))
 
-// tells Express to serve static content from the public directory
 app.use(express.static("public"))
 
-// cookie-parser => ki m mere server se user ka browser haina uski  andar ki cookies access kar paau or set kr paau => basically crud operations perform kr paau
 app.use(cookieParser())
 
 
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,4 @@
-// require('dotenv').config({path : './env'}) // -> from this , code also runs fine -> but not a good practice, code consistency khraab kr rha h 
-import dotenv from "dotenv" 
+ import dotenv from "dotenv" 
 import connectDB from "./DB/db.js";
 import { app } from "./app.js";
 
@@ -7,12 +6,6 @@ dotenv.config({
     path : './.env'
 })
 
-
-/* not a good practice
-function connectDB (){}
-connectDB()
-
-*/ 
 
 const port = process.env.PORT || 8000 ; 
 
@@ -30,43 +23,3 @@ connectDB() // db connection func
  .catch((err) => {
     console.log("MongoDB connection failed !!!" , err);
  })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
- Do using IIFE
-
-( async () => {
-    try {
-         
-      await  mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-
-      app.on("error", (error) => {
-        console.log("ERROR : ", error)
-        throw error
-      })
-
-      app.listen(process.env.PORT, () => {
-         console.log(`App is listening on port$(process.env.PORT)`);
-      })
-
-    } catch (error) {
-        console.log("ERROR: ",error)
-        throw error
-    }
-
-})()
-
-*/
